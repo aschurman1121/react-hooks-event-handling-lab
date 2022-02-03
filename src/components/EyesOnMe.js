77000d89b8c5e872a334a4c1f0b12a9a4ba782f4
alexandra <alexandraschurman@gmail.com>
2022-02-03 08:03:32 -0800

    commit tree 50dedf94524502eb364c907bd94d9694557eef39

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+
+function forFocus(){
+    console.log('Good!')
+}
+function forBlur(){
+    console.log('Hey! Eyes on me!')
+}
+ return (
+    <button onFocus={forFocus} onBlur={forBlur}>Eyes on me</button>
+ )
+    
+}
+
+export default EyesOnMe;
+
